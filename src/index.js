8f8021ec15332091b22d7ea103ec0265c6697e12
unknown <oladokunolayiwola01@gmail.com>
2022-05-15 09:00:46 -0700

    class comp

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//import SeasonDetail from './SeasonDetail';
 
-const App = () => {
-    return (
-    <div>Sagacity</div>
-    );
-};
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { lat: null };
+
+
+        window.navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.setState({ lat: position.coords.latitude });
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+
+    }
+
+
+
+    render() {
+        
+
+        return (
+            <div>Latitude: {this.state.lat} </div>
+        );
+    }
+
+
+
+}
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App />)
